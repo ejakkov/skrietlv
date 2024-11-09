0d648dcfc2c700a07b52beffde70eb5969c7e3b7
Ervins Jakovelis <wep22@inbox.v>
2024-11-09 22:36:41 +0200

    Add grid layout to the catalog

--- frontend/src/views/Catalog/Catalog.vue
+++ frontend/src/views/Catalog/Catalog.vue
@@ -1,18 +1,16 @@
 <template>
-  <div>
-    <h1>Catalog</h1>
+  <div class="catalog">
+    <h1>Category name</h1>
 
     <div v-if="loading">
       <p>Loading items...</p>
     </div>
-
+    
     <div v-else class="grid-container">
       <div v-for="item in items" :key="item._id" class="item-card">
         <img :src="item.imageUrl" alt="" class="item-image" />
         <h2>{{ item.name }}</h2>
-        <p>{{ item.description }}</p>
-        <p>Price: {{ item.price }}</p>
-        <router-link :to="{ name: 'Product', params: { id: item._id }}">View Product</router-link>
+        <p>{{ item.price }}€</p>
       </div>
     </div>
   </div>
@@ -20,3 +18,4 @@
 
 <script src="./Catalog.js"></script>
 <style src="./Catalog.css"></style>
+    
