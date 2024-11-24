7fa77e9ced469e7172e05f4c662070290bd9f477
Ervins Jakovelis <wep22@inbox.v>
2024-11-24 18:56:08 +0200

    Add sorting to the catalog

--- frontend/src/views/Catalog/Catalog.js
+++ frontend/src/views/Catalog/Catalog.js
@@ -1,20 +1,32 @@
 import axios from 'axios';
 import Footer from "../../components/Footer.vue";
 import SubNavBar from "@/components/SubNavbar.vue";
+import Select from 'primevue/select';
+
+import { ref } from 'vue';
 
 export default {
   components: {
     SubNavBar,
     Footer,
+    Select,
   },
   data() {
     return {
-      items: []
+      items: [],
+      sortOption: "asc",
+      options:[" a", "b"],
+      selectedCity: ref(),
+      cities: [
+          { name: 'New York', code: 'NY' },
+          { name: 'Rome', code: 'RM' },
+          { name: 'London', code: 'LDN' },
+          { name: 'Istanbul', code: 'IST' },
+          { name: 'Paris', code: 'PRS' }
+      ]
     };
   },
   created() {
-    console.log('Component created'); // Check if this logs
-
     this.fetchItems();
   },
   methods: {
@@ -25,6 +37,19 @@ export default {
       } catch (error) {
         console.error('Error fetching items:', error);
       }
-    }
-  }
+    },
+
+    sortItems() {
+      if (this.sortOption === "asc") {
+        this.items.sort((a, b) => a.price - b.price);
+      } else if (this.sortOption === "desc") {
+        this.items.sort((a, b) => b.price - a.price); 
+      }
+    },
+  },
+  watch: {
+    sortOption() {
+      this.sortItems();
+    },
+  },
 };
--- frontend/src/views/Catalog/Catalog.vue
+++ frontend/src/views/Catalog/Catalog.vue
@@ -3,17 +3,30 @@
     <SubNavBar />
     <h1>Catalog</h1>
 
+    <!-- <div>
+      <label for="sort">Sort by: </label>
+      <select id="sort" v-model="sortOption">
+        <option value="asc">Price: Low to High</option>
+        <option value="desc">Price: High to Low</option>
+      </select>
+    </div> -->
+    <div class="card flex justify-content-center">
+      <Select v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select a City" checkmark :highlightOnSelect="false" class="w-full md:w-56" />
+    </div>
     <div v-if="loading">
       <p>Loading items...</p>
     </div>
-    
-    <div v-else class="grid-container">
-      <div v-for="item in items" :key="item._id" class="item-card">
-        <img :src="item.imageUrl" alt="" class="item-image" />
-        <h2>{{ item.name }}</h2>
-        <p>{{ item.price }}€</p>
-      </div>
+
+    <div v-else class="catalog">
+      <div class="grid-container">
+        <div v-for="item in items" :key="item._id" class="item-card">
+          <img :src="item.imageUrl" alt="" class="item-image" />
+          <h2>{{ item.name }}</h2>
+          <p>{{ item.price }}€</p>
+        </div>
+      </div>      
     </div>
+
   </div>
   <Footer />
 </template>
